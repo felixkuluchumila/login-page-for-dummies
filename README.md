af92012005e9bbead8f06d6ac3dd41d832ad7171
felixkuluchumila <95026049+felixkuluchumila@users.noreply.github.com>
2023-05-30 23:10:02 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# login-page-for-dummies
+# Login Page for Dummies 🤓
+
+Welcome to the Login Page for Dummies project! This project aims to make the login process as easy as pie, even for the dummies out there. 😄
+
+## Features
+
+- Simple and user-friendly login form with PHP, CSS, and JS.
+- Captivating star animation to keep you entertained while logging in. ✨✨✨
+- Hilarious output that recapitulates your information in a witty way. 🤣
+- A touch of magic to transform your password into a hashed version. 🔒💫
+
+## How to Use
+
+1. Clone the repository:
+`git clone https://github.com/your-username/login-page-for-dummies.git`
+
+
+
+2. Open the `index.php` file in your favorite web browser.
+
+3. Enjoy the amusing login experience and explore the wonderful features!
+
+## Contributing
+
+If you have any funny ideas or amazing improvements to make this login page even more delightful, we welcome your contributions. Feel free to fork the repository, make your changes, and submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to use it, laugh at it, and share it with your friends. 😄
+
+---
+
+So, what are you waiting for? Dive into the world of dummy-friendly login pages and have a blast! If you encounter any issues or need assistance, don't hesitate to reach out. Happy logging! 🎉
---
+++ register.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="star.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $firstname = $_POST['fist_name'];
+        $last_name = $_POST['last_name']; 
+        $location = $_POST['location'];
+        $gender = $_POST['gender'];
+        $course = $_POST['course'];
+        $password = $_POST['upassword'];
+         echo '<div class="holder"><div class="home"><p>Hello Welcome on Our Site Here is a quick recap of Your information.<br/>
+        Username: <i>'.$firstname.' '.$last_name.' </i><br>And Your Home Address is: <i>'.$location.'<br/></i>
+        And Your Gender Is: <i>'.$gender.'</i><br/>
+        And You are Majoring: <i>'.$course.'</i><br/>
+        Your Password Is: <i>'.sha1($password).'</i><br/>
+        Hahahah, wondering what that was, its Your Password, the hashed Version, wish you all the best on our Site!</p></div>
+        <div class="wrapper">
+        <!-- <p>Happy coding</p> -->
+            <div class="square" id="s0"></div>
+            <div class="square" id="s1"></div>
+            <div class="square" id="s2"></div>
+            <div class="square" id="s3"></div>
+            <div class="square" id="s4"></div>
+            <div class="square" id="s5"></div>
+            <div class="square" id="s6"></div>
+            <div class="square" id="s7"></div>
+            <div class="square" id="s8"></div>
+            <div class="square" id="s9"></div>
+            <div class="square" id="s10"></div>
+            <div class="square" id="s11"></div>
+            <div class="square" id="s12"></div>
+            <div class="square" id="s13"></div>
+            <div class="square" id="s14"></div>
+            <div class="square" id="s15"></div>
+            <div class="square" id="s16"></div>
+            <div class="square" id="s17"></div>
+            <div class="square" id="s18"></div>
+            <div class="square" id="s19"></div>
+            <div class="square" id="s20"></div>
+            <div class="square" id="s21"></div>
+            <div class="square" id="s22"></div>
+            <div class="square" id="s23"></div>
+            <div class="square" id="s24"></div>
+            <div class="square" id="s25"></div>
+            <div class="square" id="s26"></div>
+            <div class="square" id="s27"></div>
+            <div class="square" id="s28"></div>
+            <div class="square" id="s29"></div>
+    </div>  
+        
+        <h2>Have A wonderful Experience, Call this Place Home 💘💘💘💘</h2>
+        ';
+        
+    ?>
+</body>
+</html>
